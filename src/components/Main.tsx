93f3b50c3720fdc9b8250bcf62b8f67f88c30d94
xbexbex <petja.valkama@gmail.com>
2018-12-24 23:43:01 +0200

    Update editorconfig

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,18 +3,18 @@ import 'Styles/Main.scss';
 
 export default class Main extends Component {
 
-    constructor(props: {}) {
-        super(props)
-    }
+  constructor(props: {}) {
+    super(props)
+  }
 
-    render() {
-        return (
-            <div className="main-container">
-                <p>Hi</p>
-                <img
-                    src={require('Assets/loading.svg')}>
-                </img>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="main-container">
+        <p>Hi</p>
+        <img
+          src={require('Assets/loading.svg')}>
+        </img>
+      </div>
+    )
+  }
 }
