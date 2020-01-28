456c00f9b77759d1697b631cb9d6c926e53110c0
xbexbex <petja.valkama@gmail.com>
2020-01-28 12:39:43 +0200

    Add support for env vars

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,16 @@ import { ThemeProvider } from 'styled-components';
 import { Theme, GlobalStyle } from '../styles';
 import Main from './Main';
 
+// This comes from babel.config.js and can be declared anywhere
+// declare const API_KEY
 
-const App: React.FC<{}> = ({ }) => {
+const App: React.FC<{}> = ({}) => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
       <Main></Main>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
